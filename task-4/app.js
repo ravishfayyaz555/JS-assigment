27d7487ce6cf564c259bdd13453e55f186a86995
Ravish Fayyaz <ravishfayyaz13@gmail.com>
2026-07-27 13:20:37 +0500

    Update JavaScript project

---
+++ task-4/app.js
@@ -0,0 +1,41 @@
+// q no 1
+
+var a , b , c ;
+
+// question no 2
+
+// legal variable 
+var name = "Ravish Fayyaz";
+
+var myNum = 24 ;
+
+var _student = true;
+
+var $rupees = "5dollars";
+
+var batch21 = "Web abd dev";
+
+document.write("<br/>")
+
+// illegal variables
+// var 3num = 23 ;
+
+// var my name = "ravish fayyaz";
+
+// var course-name ;
+
+// var user@ ;
+
+// var for ;
+
+// question no 3
+
+document.write("Rules for naming JS variables <br />"); 
+
+document.write("variables name can onley contain $ , _undescore, numbers . for example $my_1st variable<br />");
+document.write("Variable must being with a Letters , $ and _.for example $name,_name,or name.<br />");
+
+document.write("Variable name are caseSensative.<br />");
+document.write("Variable name should not be JS Keyword.")
+
+
---
+++ task-5/app.js
@@ -0,0 +1,106 @@
+// question no 1
+
+var num1 = 3 ;
+var num2 = 5 ;
+var total = num1 + num2;
+document.write("Sum of 3 and 5 is ");
+document.write(total);
+
+// question no 2
+
+var num1 = 3 ;
+var num2 = 5 ;
+var total = num1 - num2;
+document.write("Subtraction of 3 and 5 is <br/>");
+document.write(total);
+
+var num1 = 3 ;
+var num2 = 5 ;
+var total = num1 * num2;
+document.write("Multiply of 3 and 5 is <br/>");
+document.write(total);
+
+var num1 = 3 ;
+var num2 = 5 ;
+var total = num1 / num2;
+document.write("devision of 3 and 5 is <br/> ");
+document.write(total);
+
+var num1 = 3 ;
+var num2 = 5 ;
+var total = num1 % num2;
+document.write("modulus of 3 and 5 is <br/> ");
+document.write(total);
+document.write("<br/>");
+
+// question no 3
+
+var myVari
+var myVari = "Value after variablr declearation is:?? ";
+document.write(myVari);
+document.write("<br/>");
+var myVari = 5 ;
+document.write("Initial valur: " + myVari);
+document.write("<br/>");
+ myVari++ ; //increment keya hai 
+ document.write(myVari);
+ document.write("<br/>");
+
+document.write("Increment the variable " ,myVari);
+document.write("<br/>");
+
+myVari = myVari + 7 ;
+document.write("Value after adding is : ",myVari, "<br/>");
+
+myVari = --myVari ;
+document.write(myVari, "<br/>");
+
+document.write("Value after decrement is :", myVari);
+
+document.write("<br/>");
+document.write("Output : The reminder is :",+ (myVari % 3));
+document.write("<br/>");
+
+var newPart ;
+document.write(newPart);
+document.write("<br/>");
+document.write("Value after declearation variable is :",newPart);
+document.write("<br/>");
+document.write("Initial value is :5");
+document.write("<br/>");
+document.write("Value after increment is : 6");
+document.write("<br/>");
+document.write("Value after addition is : 13");
+document.write("<br/>");
+document.write("Value after decrement is: 12");
+document.write("<br/>");
+document.write("The reminder is : 0");
+document.write("<br/>");
+// question no 4
+
+var movieTicket = 600;
+
+document.write(movieTicket*5);
+document.write("<br/>");
+document.write("Total cost of buy tickets to movie tickets: ", movieTicket," PKR");
+document.write("<br/>");
+
+// question no 5
+
+// var table = 4;
+// table = table +1
+// document.write("Table of 4");
+// document.write(table+"x"+count+"="+table);  // ise thek kr k solve krna hai
+
+document.write("<br/>");
+// question no 6
+
+var celsius = 25°C;
+var farhenhite = celsius * 9/5;
+document.write(celsius+"°C" ,farhenhite+"°F");
+
+
+
+
+
+
